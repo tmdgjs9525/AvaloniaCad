a28a31c41c74ba5ad6882d2af0e96b3ce66ca64b
SeungHeon <56830330+tmdgjs9525@users.noreply.github.com>
2026-09-14 16:10:30 +0900

    feat : ViewManager 붙이기

---
+++ ViewManager/Avalonia.Container/Dialog/DialogBase.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia.Container.Util;
+using EyeContainer.Core.Interfaces;
+using Avalonia;
+using Avalonia.Animation; // Animation, KeyFrame
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input; // PointerPressedEventArgs
+using Avalonia.Interactivity;
+using Avalonia.Media; // ScaleTransform
+using Avalonia.Styling;
+
+namespace Avalonia.Container;
+
+public partial class DialogBase : Window
+{
+    private readonly StartPosition? _startPosition;
+    public DialogBase()
+    {
+        InitializeComponent();
+
+        WindowStartupLocation = WindowStartupLocation.CenterOwner;
+    }
+
+    public DialogBase(StartPosition? startPosition)
+        : this()
+    {
+        _startPosition = startPosition;
+    }
+}
