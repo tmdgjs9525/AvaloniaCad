1bf043625ead958d31de30ad8aa11df4ef17271b
SeungHeon <56830330+tmdgjs9525@users.noreply.github.com>
2026-09-15 17:59:42 +0900

    feat : KoDrawing.Core 추가

---
+++ KoDrawing.Core/ViewportTransform.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace KoDrawing.Core;
+
+public sealed class ViewportTransform
+{
+    public Vector2 CameraPosition { get; set; }
+
+    public float Zoom { get; set; } = 1.0f;
+
+    public Vector2 ViewportCenter { get; set; }
+
+    public Vector2 WorldToScreen(Vector2 world)
+    {
+        return (world - CameraPosition) * Zoom
+               + ViewportCenter;
+    }
+
+    public Vector2 ScreenToWorld(Vector2 screen)
+    {
+        return (screen - ViewportCenter) / Zoom
+               + CameraPosition;
+    }
