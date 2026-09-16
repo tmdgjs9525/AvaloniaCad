b413a6f41860de26a9bc30fc2e921673e91b95d5
SeungHeon <56830330+tmdgjs9525@users.noreply.github.com>
2026-09-16 14:52:02 +0900

    tests : ViewPortTransformTest 추가

---
+++ KoDrawing.Tests/Viewport/ViewportTransformTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using KoDrawing.Core;
+using Xunit;
+
+namespace KoDrawing.Tests.Viewport;
+
+public class ViewportTransformTests
+{
+    [Fact]
+    public void WorldToScreen_And_ScreenToWorld_ReturnsOriginalPoint()
+    {
+        var transform = new ViewportTransform
+        {
+            CameraPosition = new Vector2(100, 50),
+            Zoom = 2.0f,
+            ViewportCenter = new Vector2(500, 300)
+        };
+
+        var world = new Vector2(150, 100);
+
+        var screen = transform.WorldToScreen(world);
+        var result = transform.ScreenToWorld(screen);
+
+        Assert.Equal(world, result);
+    }
+}
