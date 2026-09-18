afdb3830e46e664ebd12d48650bedec3c4a45b34
SeungHeon <56830330+tmdgjs9525@users.noreply.github.com>
2026-09-18 11:17:33 +0900

    feat : DrawingCanvas 추가, 크기변경마다 Viewport 갱신

---
+++ AvaloniaCad.Editor/Controls/DrawingCanvas.axaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using Avalonia.Controls;
+using KoDrawing.Core;
+
+namespace AvaloniaCad.Editor.Controls;
+
+public partial class DrawingCanvas : UserControl
+{
+    public ViewportTransform Viewport { get; } = new();
+    
+    public DrawingCanvas()
+    {
+        InitializeComponent();
+        
+        SizeChanged += OnSizeChanged;
+    }
+
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        Viewport.ViewportCenter = new Vector2(
+            (float)(e.NewSize.Width / 2),
+            (float)(e.NewSize.Height / 2));
+    }
+}
