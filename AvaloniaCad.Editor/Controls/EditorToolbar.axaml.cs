20dae722b6277638b04eb45f34b60322852ac603
tmdgjs9525 <56830330+tmdgjs9525@users.noreply.github.com>
2026-09-20 17:19:18 +0900

    feat : Toolbar UI

---
+++ AvaloniaCad.Editor/Controls/EditorToolbar.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaCad.Editor.Controls;
+
+public partial class EditorToolbar : UserControl
+{
+    public EditorToolbar()
+    {
+        InitializeComponent();
+    }
+}
